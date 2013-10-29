fc1d29cfd0c29a09044f382b1d21cfd32754ab22
hukuuu <hukuuu@gmail.com>
2013-10-30 00:05:31 +0200

    word guessing functionallity added

--- app/js/directives/word/word.js
+++ app/js/directives/word/word.js
@@ -1,5 +1,6 @@
 angular.module('directives')
   .directive('word', function() {
+	  var ENTER_KEYCODE = 13;
 	  console.log('making word directive');
     return {
       restrict: 'E',
@@ -7,9 +8,30 @@ angular.module('directives')
       scope: {
 	      word: '='
       },
+	replace:true,
       templateUrl: 'js/directives/word/word.html',
 	link: function(scope, element, attrs){
-		console.log('word directive link function');
+		scope.placeholder = 'guess \u21B5';
+		scope.guess="";
+		scope.success = false;
+		scope.fail = false;
+		scope.toggle = function(){
+			scope.isOpen = !scope.isOpen;
+		};
+		scope.keyPressed = function(event) {
+			if(event.keyCode === ENTER_KEYCODE) {
+				if (scope.word.bulgarianValues.indexOf(scope.guess.trim())>-1) {
+					console.log('pozna');
+					scope.success = true;
+					scope.fail = false;
+
+				} else {
+					console.log('ne pozna');
+					scope.success = false;
+					scope.fail = true;
+				}
+			}
+		}
 	}
     };
   });
