8106f82df5f809db1828f78b57a67f8ea89463ec
hukuuu <hukuuu@gmail.com>
2013-10-30 13:02:45 +0200

    added handler to refresh event to search again for the last term searched

--- app/js/modules/list/controller.js
+++ app/js/modules/list/controller.js
@@ -1,14 +1,26 @@
 angular.module('list',[])
 	.controller('ListCtrl',['$scope','WordService','$rootScope',function($scope,wordService,$rootScope){
 		console.log('list controller working....',wordService);
+
+		var lastTerm;
+
 		$rootScope.$on('search',function(event,term){
+			search(term);
+		});
+		$rootScope.$emit('search','');
+
+		$scope.refresh = function(){
+			search(lastTerm);
+		}
+
+
+		function search(term){
 			console.log('list controller shoulda search...',term);
+			lastTerm = term;
 			wordService.list(term)
 			.then(function(payload){
 				$scope.words = payload.data
 			});
-
-		});
-		$rootScope.$emit('search','');
+		}
 
 	}]);
