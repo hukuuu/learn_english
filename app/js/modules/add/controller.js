6dd0410f9eade0167e40ec29a2969b41d0fe8245
nickolayrusev <nickolay.rusev@gmail.com>
2013-10-31 19:20:05 +0200

    typeahead running with hardcoded values , needs to be implemented

--- app/js/modules/add/controller.js
+++ app/js/modules/add/controller.js
@@ -1,9 +1,12 @@
-angular.module('add',[])
+angular.module('add',['ui.bootstrap'])
 	.controller('AddCtrl',['$scope','WordService',function($scope,wordService){
 		console.log('add controller is working',$scope);
 		$scope.word = {};
+		
+		
 		$scope.word = {
-			bulgarianValues: [{index:1,value:''}]
+			bulgarianValues: [{index:1,value:''}],
+			states : ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 		}
 		$scope.onOk = function(){
 			var word = {
