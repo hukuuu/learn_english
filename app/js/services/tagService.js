1818f916fdbbddd69768eda50778711eab09bc95
nikolay rusev <nickolay.rusev@gmail.com>
2013-11-01 14:23:49 +0200

    now tag functionality working as expected

--- app/js/services/tagService.js
+++ app/js/services/tagService.js
@@ -2,8 +2,8 @@ angular.module('services')
 	.factory('TagService', ['$http',function($http){
 		function list () {
 			console.log('in tag service');
-			return ["Alaska","Los Angeles","San Diego"];
-			//return $http.get('http://learnenglishonline.herokuapp.com/words.json',{params:params});
+			//return ["Alaska","Los Angeles","San Diego"];
+			return $http.get('http://learnenglishonline.herokuapp.com/tags.json');
 		}
 		return {
 			list: list
