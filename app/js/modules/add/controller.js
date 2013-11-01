34f2985e4efc391857ab54bb11685b35c278dd38
nikolay rusev <nickolay.rusev@gmail.com>
2013-11-01 13:22:27 +0200

    implemented fully functional tags. add and delete

--- app/js/modules/add/controller.js
+++ app/js/modules/add/controller.js
@@ -2,11 +2,10 @@ angular.module('add',['ui.bootstrap'])
 	.controller('AddCtrl',['$scope','WordService','TagService',function($scope,wordService,tagService){
 		console.log('add controller is working',$scope);
 		$scope.word = {};
-		
+		$scope.states = tagService.list();
 		
 		$scope.word = {
 			bulgarianValues: [{index:1,value:''}],
-			states : tagService.list(),
 			tags:[]
 		}
 		$scope.onOk = function(){
@@ -25,8 +24,10 @@ angular.module('add',['ui.bootstrap'])
 			}
 			$scope.word.selected='';
 		}
-		$scope.removeTag  = function (event) {
-			var tagText = event.target.innerText.trim();
+		$scope.removeTag  = function (tag) {
+			console.log(tag);
+			//var tagText = event.target.attributes["data-tag"].nodeValue;
+			var tagText = tag;
 			console.log("removeTag",tagText);
 			var indexOfText = $scope.word.tags.indexOf(tagText);
 			$scope.word.tags.splice(indexOfText,1);
