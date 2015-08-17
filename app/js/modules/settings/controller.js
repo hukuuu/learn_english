c7a9611e93e3d42b25e34fdcef1734671e419f4b
hukuuu <hukuuu@gmail.com>
2015-08-17 12:01:58 +0300

    notification on settings saved

--- app/js/modules/settings/controller.js
+++ app/js/modules/settings/controller.js
@@ -1,10 +1,11 @@
 angular.module('settings',[])
-	.controller('SettingsCtrl',['$scope', 'HintService', 'WordService', function($scope, hintService, wordService){
+	.controller('SettingsCtrl',['$scope', 'HintService', 'WordService', 'NotificationService', function($scope, hintService, wordService, notificationService){
 		console.log('settings ctrl');
 		$scope.hintPercentage = hintService.getPercentage();
 		$scope.wordsPerPage =  wordService.getWordsPerPage();
 		$scope.save = function(){
 			hintService.setPercentage($scope.hintPercentage);
 			wordService.setWordsPerPage($scope.wordsPerPage);
+			notificationService.success("Settings saved.");
 		}
 	}]);
